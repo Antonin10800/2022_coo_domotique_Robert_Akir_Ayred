eb5f578eddb4771845becdd7ff33dbc01f76cf1d
Antonin Robert <anto10800@gmail.com>
2022-04-05 11:11:22 +0200

    Ajout squelette Telecommande

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+
 public class Telecommande {
 
+    ArrayList<Lampe> list = new ArrayList<>();
+/*
+Constructrices
+ */
+/*
+  Méthodes
+*/
+
+
+    public void activerTour(){
+        throw new Error (" code non ecrit ");
+    }
+    public void ajouterLampe(Lampe l){
+        throw new Error (" code non ecrit ");
+    }
+    public void activerLampe(int indiceLampe){
+        throw new Error (" code non ecrit ");
+    }
+    public void desactiverLampe(int indiceLampe){
+        throw new Error (" code non ecrit ");
+    }
+    @Override
+    public String toString() {
+        throw new Error (" code non ecrit ");
+    }
 }
