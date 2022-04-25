b6ddd802f5ebd40d7ecff33388b7bb6c0b51ba07
Antonin Robert <anto10800@gmail.com>
2022-04-25 15:15:24 +0200

    Modif des shema ULM

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 2022_coo_domotique_Robert_Akir_Ayred
+
+TD 07 : 
+    Question 3 : 
+        Pour gérer les Hifi, on a du créer une nouvelle ArrayList de<Hifi> 
+        et ajouter les méthodes. 
+        1 ligne de variable
+        2 méthodes
+
+    Question 4 : 
+    
--- src/Telecommande.java
+++ src/Telecommande.java
@@ -3,60 +3,67 @@ import java.util.ArrayList;
 public class Telecommande {
 
     ArrayList<Lampe> list;
-/*
-Constructeur
- */
-    public Telecommande(){
+    ArrayList<Hifi> listHifi;
+
+    /*
+    Constructeur
+     */
+    public Telecommande() {
         list = new ArrayList<>();
+        listHifi = new ArrayList<>();
     }
-/*
-  Méthodes
-*/
-    public void allumerHifi(int indiceList){
+
+    /*
+      Méthodes
+    */
+    public void allumerHifi(int indiceList) {
         //Hifi hifi = new Hifi();
-        list.get(indiceList).allumer();
+        listHifi.get(indiceList).allumer();
     }
-    public void eteindreHifi(int indiceList){
+
+    public void eteindreHifi(int indiceList) {
         //Hifi hifi = new Hifi();
-        list.get(indiceList).eteindre();
+        listHifi.get(indiceList).eteindre();
     }
 
 
-
-    public void activerTout(){
+    public void activerTout() {
         int taille = list.size();
-        for(int i =0; i<taille; i++){
+        for (int i = 0; i < taille; i++) {
             activerLampe(i);
         }
 
     }
-    public void ajouterLampe(Lampe l){
-        if(l != null){
+
+    public void ajouterLampe(Lampe l) {
+        if (l != null) {
             list.add(l);
         }
     }
-    public void activerLampe(int indiceLampe){
+
+    public void activerLampe(int indiceLampe) {
 
         Lampe lampe2 = new Lampe("test");
         try {
             list.get(indiceLampe).allumer();
-        }catch (IndexOutOfBoundsException e2){
+        } catch (IndexOutOfBoundsException e2) {
 
         }
 
 
     }
-    public void desactiverLampe(int indiceLampe){
+
+    public void desactiverLampe(int indiceLampe) {
         Lampe lampe = new Lampe("test");
         try {
             list.get(indiceLampe).eteindre();
-        }catch (IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
 
         }
 
 
-
     }
+
     @Override
     public String toString() {
         return "Telecommande{" +
